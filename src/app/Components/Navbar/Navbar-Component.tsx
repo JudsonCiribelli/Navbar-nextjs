3d72c2e15cdcb91106c1d68e803c134d26519ec6
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-31 18:16:31 -0300

    style: add styless to Nav-items Component

--- src/app/Components/Navbar/Navbar-Component.tsx
+++ src/app/Components/Navbar/Navbar-Component.tsx
@@ -1,17 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
-import NavItem from "../NavItem/NavItem-Component";
+//Components
+import NavItem, { NavItemInterface } from "../NavItem/NavItem-Component";
+//Styles
+import "./Navbar-Styles.css";
 
 export default function Navbar() {
+  const Items: NavItemInterface[] = [
+    {
+      url: "/",
+      label: "Ínicio",
+    },
+    {
+      url: "/about",
+      label: "Sobre",
+    },
+
+    {
+      url: "/products",
+      label: "Produtos",
+    },
+    {
+      url: "/services",
+      label: "Serviços",
+    },
+    {
+      url: "/contacts",
+      label: "Contatos",
+    },
+  ];
+
   return (
     <header>
-      <nav>
-        <Link href="/">
-          <Image src="Logo.jpg.svg" width={40} height={40} alt="Minha_Logo" />
+      <nav className="navbar">
+        <Link href="/" className="logo">
+          <Image
+            src="2qzsQ1RdlRhMi8b2ML025BWQ2OU.svg"
+            width={40}
+            height={40}
+            alt="Minha_Logo"
+          />
         </Link>
         <ul className="nav-items">
-          <NavItem url="/" label="Ínicio" />
+          {Items.map((item, index) => (
+            <NavItem url={item.url} label={item.label} key={index} />
+          ))}
         </ul>
+        <button className="btn-default">Contatar</button>
       </nav>
     </header>
   );
